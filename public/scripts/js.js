f2c1b45ddabe4359ceef4f66491a1f677308e3b3
hieurl <luongtronghieu@gmail.com>
2012-11-29 17:28:52 +0100

    implement change track function

--- public/scripts/js.js
+++ public/scripts/js.js
@@ -1,5 +1,23 @@
 //Show new event
 $(document).ready(function() {
+	$('a.track_window').click(function() {
+		var trackBox = $(this).attr('href');
+		$(trackBox).fadeIn(300);
+
+		var popMargTop = ($(trackBox).height() + 24) / 2;
+		var popMargLeft = ($(trackBox).height() + 24) / 2;
+		
+		$(trackBox).css({
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+
+		$('body').append('<div id="mask"></div>');
+		$('#mask').fadeIn(300);
+		
+		return false;
+	});
+
 	$('a.event_window').click(function() {
 		
         //Getting the variable's value from a link 
@@ -25,11 +43,17 @@ $(document).ready(function() {
 	});
 	
 	// When clicking on the button close or the mask layer the popup closed
-	$('a.close, #mask').live('click', function() { 
-	  $('#mask , .event_popup').fadeOut(300 , function() {
-		$('#mask').remove();  
-	}); 
-	return false;
+	$('.close, #mask').live('click', function() { 
+		$('#mask , #new_event').fadeOut(300 , function() {
+			$('#mask').remove();  
+		}); 
+		
+		$('#mask , #choose_track').fadeOut(300 , function() {
+			$('#mask').remove();
+        });
+
+		
+		return false;
 	});
 	
 	//script for auto-complete course input
