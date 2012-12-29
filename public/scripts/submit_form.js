5ab2211fe739daa8659cc79fcb5fb48f9cd41d40
Ghassane <ghassane@ghassane.(none)>
2012-12-29 21:18:45 +0100

    Submission login, redirection to main.html.

---
+++ public/scripts/submit_form.js
@@ -0,0 +1,19 @@
+var loginList=["amchyaa@eurecom.fr","ghassane.amchyaa@eurecom.fr"]
+var password="webint"
+
+function include(arr,obj) {
+    return (arr.indexOf(obj) != -1);
+}
+
+function submit_form() {
+    var login=document.getElementById('login').value
+    var pass=document.getElementById('passwrd').value
+    if ((include(loginList,login)) && (pass==password)) {
+	document.location.href="./main.html"
+    }
+    else {
+	alert("Try Again")
+    }
+}
+
+document.getElementById('subm').addEventListener("click",submit_form,true)
