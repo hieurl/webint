69ed7fbdcd44f69a9a955fdc7adea17678030e64
Ghassane <ghassane@ghassane.(none)>
2012-12-31 13:38:57 +0100

    Css to the rating function

--- public/scripts/add_comment.js
+++ public/scripts/add_comment.js
@@ -16,6 +16,16 @@ function modifyCss() {
 	var user=document.getElementById('user'+i)
 	var time=document.getElementById('time'+i)
 	var button=document.getElementById(i)
+	var rate=document.getElementById("comment_rate"+i)
+	var rate_num=document.getElementById("rate_num"+i)
+	//var commentaire=document.getElementById("commentaire"+i)
+	//rate number
+	rate_num.style.textAlign="center"
+	rate_num.style.fontSize="17px"
+	//comment rate
+	rate.style.cssFloat="left"
+	rate.style.marginLeft="30px"
+	//rate.style.marginTop="20px"
 	//user
 	user.style.fontStyle="italic"
 	user.style.fontSize="10px"
@@ -37,7 +47,7 @@ function modifyCss() {
 	comment.style.fontSize="15px"
 	i++
 	//button
-	button.style.marginLeft="500px"
+	button.style.marginLeft="427px"
     }
 }
 
@@ -62,19 +72,18 @@ function update_comments() {
 	count++
 	//add comment
 	text_comment.innerHTML="<textarea class=\"comment\" id=\"comment\" placeholder=\"Post your comment...\" ></textarea>"
-	var to_add="<div id=\"comment_post"+count+"\">"+
-	'<div>'+
+	var to_add='<div id="commentaire"'+count+'><div id="comment_rate'+count+'">'+
 	  '<a class="comment_up" href="plus1" title="Rate up!">'+
 	    '<img src="./images/arrow_up_gray.png" alt="up arrow" >'+
 	  '</a>'+
-	  '<div class="comment_rate_num">0</div>'+
+	  '<div id="rate_num'+count+'" class="comment_rate_num">0</div>'+
 	  '<span class="voted" hidden="hidden" >not</span>'+
 	  '<a class="comment_down" href="minus1" title="Rate down!">'+
 	    '<img src="./images/arrow_down_gray.png" alt="up arrow" >'+
 	  '</a>'+
-	'</div>'+
+	'</div>'+"<div id=\"comment_post"+count+"\">"+
 	commentToAdd+
-	"<div id=\"user"+count+"\">posted by user</div><div id=\"time"+count+"\">"+month + "/" + day + "/" + year+" @ "+hours+":"+minutes+"</div></div><input type=\"button\" id=\""+count+"\" class=\"post\" value=\"DELETE\" />"
+	"<div id=\"user"+count+"\">posted by user</div><div id=\"time"+count+"\">"+month + "/" + day + "/" + year+" @ "+hours+":"+minutes+"</div></div><input type=\"button\" id=\""+count+"\" class=\"post\" value=\"DELETE\" /></div>"
 	//array_messages.push(to_add)
 	str=to_add+str
 	article.innerHTML=str
