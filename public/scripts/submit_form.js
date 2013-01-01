d70e8fa3e16d7d455a9e1c4d7c3215dd4c46522e
Ghassane <ghassane@ghassane.(none)>
2013-01-01 15:50:10 +0100

    Nothing special

--- public/scripts/submit_form.js
+++ public/scripts/submit_form.js
@@ -1,10 +1,13 @@
+// a simple checkin of the users with one user. Me :-)
 var loginList=["amchyaa@eurecom.fr","ghassane.amchyaa@eurecom.fr"]
 var password="webint"
 
+//verify if an object is the array or not
 function include(arr,obj) {
     return (arr.indexOf(obj) != -1);
 }
 
+//if the user is Me, redirect to the home(main) page
 function submit_form() {
     var login=document.getElementById('login').value
     var pass=document.getElementById('passwrd').value
